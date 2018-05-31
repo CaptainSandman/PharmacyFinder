b77a69fbf612a3450bbbb0903f26a3805f5e5fc9
Josh Kennedy <allamericanjbert@gmail.com>
2018-05-30 21:29:04 -0500

    added copyright info to about page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -3,6 +3,8 @@
     <b-container>
       <h1>About PharmacyFinder</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel omnis, voluptates facere expedita possimus asperiores laborum illo exercitationem cupiditate libero provident doloremque facilis vitae dignissimos quae sint hic ullam quo.</p>
+      <br/>
+      <p>Copyright &copy; 2018 Josh Kennedy. All rights reserved.</p>
     </b-container>
   </div>
 </template>
